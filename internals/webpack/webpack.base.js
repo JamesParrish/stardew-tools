97c2f07d2fd3df19a99f1d684788fd7fc13069de
James P <crazybobby@gmail.com>
2020-04-18 17:45:35 +0100

    Added Typescript

--- internals/webpack/webpack.base.js
+++ internals/webpack/webpack.base.js
@@ -10,9 +10,22 @@ const plugins = [
 
 module.exports = (options) => ({
   mode: options.mode,
-  entry: [path.join(process.cwd(), 'src/index.js')],
+  entry: [path.join(process.cwd(), 'src/index.tsx')],
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.js', '.ts', '.tsx', '.json'],
+  },
   module: {
     rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
